54385d15f685bd17f2c79c529b07d0da38899dd1
Bhupesh Sharma <bhupesh.sharma@freescale.com>
2015-08-19 19:57:04 +0530

    LS1043aRdbPkg/Duart: Fix Licensing issues
    This patch fixes the licensing clause in DUART PEI
    and DXE library/driver layers.
    
    Signed-off-by: Bhupesh Sharma <bhupesh.sharma@freescale.com>

--- LS1043aRdbPkg/Drivers/DUart/DUart.c
+++ LS1043aRdbPkg/Drivers/DUart/DUart.c
@@ -1,17 +1,15 @@
 /** Duart.c
   Duart driver
 
-Copyright (c) 2013, Freescale Ltd. All rights reserved.
-Author: Bhupesh Sharma <bhupesh.sharma@freescale.com>
+  Copyright (c) 2015, Freescale Semiconductor, Inc. All rights reserved.
 
-This program and the accompanying materials
-are licensed and made available under the terms and conditions of the BSD License
-which accompanies this distribution.  The full text of the license may be found at
-http://opensource.org/licenses/bsd-license.php
+  This program and the accompanying materials
+  are licensed and made available under the terms and conditions of the BSD License
+  which accompanies this distribution.  The full text of the license may be found at
+  http://opensource.org/licenses/bsd-license.php
 
-THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
-WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
-
+  THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
+  WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
  **/
 
 #include <LS1043aSocLib.h>
