2cf608095788a3a774422b921d87be93f5736c96
Sakar Arora <Sakar.Arora@freescale.com>
2015-08-20 15:20:54 +0530

    LS1043aRdbPkg/PrePiNor: Licensing information corrected
    Signed-off-by: Sakar Arora <Sakar.Arora@freescale.com>

--- LS1043aRdbPkg/Library/LS1043aPrePiNor/LS1043aPrePiNor.c
+++ LS1043aRdbPkg/Library/LS1043aPrePiNor/LS1043aPrePiNor.c
@@ -1,3 +1,18 @@
+/** @LS1043aPrePiNor.c
+#
+#  Copyright (c) 2015, Freescale Semiconductor, Inc. All rights reserved.
+#
+#  This program and the accompanying materials
+#  are licensed and made available under the terms and conditions of the BSD License
+#  which accompanies this distribution.  The full text of the license may be found at
+#  http://opensource.org/licenses/bsd-license.php
+#
+#  THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
+#
+**/
+
+
 #include <Library/ArmLib.h>
 #include <Library/PcdLib.h>
 #include <LS1043aRdb.h>
