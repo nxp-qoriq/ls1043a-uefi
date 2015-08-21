381e11ef92af36f6d744e7b9109e98e544741184
Sakar Arora <Sakar.Arora@freescale.com>
2015-08-21 21:07:04 +0530

    LS1043aRdb/RTC: Copyright information updated
    Signed-off-by: Sakar Arora <Sakar.Arora@freescale.com>

--- LS1043aRdbPkg/Library/Ds1307RtcLib/Ds1307RtcLib.c
+++ LS1043aRdbPkg/Library/Ds1307RtcLib/Ds1307RtcLib.c
@@ -1,8 +1,10 @@
 /** @Ds1307RtcLib.c
   Implement EFI RealTimeClock with runtime services via RTC Lib for DS1307 RTC.
 
-  Currently this driver does not support runtime virtual calling.
+	Based on RTC implementation available in
+  EmbeddedPkg/Library/TemplateRealTimeClockLib/RealTimeClockLib.c
 
+  Copyright (c) 2008 - 2009, Apple Inc. All rights reserved.<BR>
   Copyright (c) 2015, Freescale Semiconductor, Inc. All rights reserved.
 
   This program and the accompanying materials
