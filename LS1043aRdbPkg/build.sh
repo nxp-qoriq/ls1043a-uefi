20f91f320085a39cbe21b2e662ec1db6af8e951f
Bhupesh Sharma <bhupesh.sharma@freescale.com>
2015-09-07 15:35:43 +0530

    build-script: Allow passing mkimage utility path as an argument to the script
    This patch modifies the build-script to allow passing mkimage utility path
    as an argument to the script.
    
    Signed-off-by: Bhupesh Sharma <bhupesh.sharma@freescale.com>

--- LS1043aRdbPkg/build.sh
+++ LS1043aRdbPkg/build.sh
@@ -22,6 +22,7 @@ print_usage_banner()
 		we build the XIP candidate. FATXIP is used for FAT
 		filesystem based XIP boot case, while NONXIP is used for
 		NAND boot currently"
+	echo "	Arg 3 (mandatory for NONXIP boot): absolute path of mkimage utility."
 	echo "	Arg 3 (optional): clean - To do a 'make clean' operation."
 }
 
@@ -54,11 +55,15 @@ fi
 if [[ $2 == "NONXIP" ]]; then
 	BootSuffix="NonXipBoot.dsc"
 	echo "Compiling for NON-XIP boot"
+	if [[ $3 == "" ]]; then
+		echo "Error ! Incorrect 3rd argument to build script."
+		print_usage_banner
+		exit
+	fi
+
 	if [[ $3 != "clean" ]]; then
-		echo "Enter the absolute path for mkimage utility: "
-		echo "(Hint: Usually this is the path of the u-boot/tools directory) "
-		read MKIMAGE_PATH
-		echo "mkimage utility is available at this path: $MKIMAGE_PATH"
+		echo "Using mkimage utility is available at this path: $3"
+		echo "(Hint: Usually this is the absolute path of the u-boot/tools directory)"
 	fi
 else
 	if [[ $2 == "XIP" ]]; then
@@ -85,9 +90,11 @@ else
 		# Do nothing as argument 3 is optional.
 		echo " "
 	else
-		echo "Error ! Incorrect 3rd argument to build script."
-		print_usage_banner
-		exit
+		if [[ $2 != "NONXIP" ]]; then
+			echo "Error ! Incorrect 3rd argument to build script."
+			print_usage_banner
+			exit
+		fi
 	fi
 fi
 
@@ -112,6 +119,6 @@ TARGET_TOOLS=GCC48
 # Actual build command
 build -p "$WORKSPACE/LS1043aRdbPkg/LS1043aRdbPkg$BootSuffix" -a $ARCH -t $TARGET_TOOLS -b $1
 if [[ $2 == "NONXIP" ]]; then
-$MKIMAGE_PATH/mkimage -n $WORKSPACE/LS1043aRdbPkg/Library/LS1043aPrePiOcram/ls1043ardb_rcw_nand.cfg -R $WORKSPACE/LS1043aRdbPkg/Library/LS1043aPrePiOcram/ls1043ardb_pbi.cfg -T pblimage -A arm -a 0x10000000 -d $WORKSPACE/Build/LS1043aRdb/$1_GCC48/FV/LS1043ARDBPI_EFI.fd $WORKSPACE/LS1043aRdbPkg/Library/LS1043aPrePiOcram/LS1043ARDBPI_EFI.pbl
+$3/mkimage -n $WORKSPACE/LS1043aRdbPkg/Library/LS1043aPrePiOcram/ls1043ardb_rcw_nand.cfg -R $WORKSPACE/LS1043aRdbPkg/Library/LS1043aPrePiOcram/ls1043ardb_pbi.cfg -T pblimage -A arm -a 0x10000000 -d $WORKSPACE/Build/LS1043aRdb/$1_GCC48/FV/LS1043ARDBPI_EFI.fd $WORKSPACE/LS1043aRdbPkg/Library/LS1043aPrePiOcram/LS1043ARDBPI_EFI.pbl
 echo "PBL image created at $WORKSPACE/LS1043aRdbPkg/Library/LS1043aPrePiOcram/LS1043ARDBPI_EFI.pbl"
 fi
