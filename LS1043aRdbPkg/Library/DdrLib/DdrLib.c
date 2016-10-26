dd00f1e84ab60df6ca22f81d30ddc6cefbed48a5
Meenakshi Aggarwal <meenakshi.aggarwal@freescale.com>
2016-10-26 15:12:01 +0530

    DdrLib: Removed stale function
    Signed-off-by: Meenakshi Aggarwal <meenakshi.aggarwal@freescale.com>

--- LS1043aRdbPkg/Library/DdrLib/DdrLib.c
+++ LS1043aRdbPkg/Library/DdrLib/DdrLib.c
@@ -18,11 +18,6 @@
 #include <I2c.h>
 #include <Library/BaseLib.h>
 
-#define Uswap32(X) \
-       ((((X) & 0xff000000) >> 24) | \
-        (((X) & 0x00ff0000) >>  8) | \
-        (((X) & 0x0000ff00) <<  8) | \
-        (((X) & 0x000000ff) << 24))
 #define DDRMC_DELAY 10000
 
 /**
