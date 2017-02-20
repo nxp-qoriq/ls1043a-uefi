nxp-qoriq/ls1043a-uefi

commit 17d39cabee3eab5935c60d7e96c272c4e1e8c4e4

LS1043A: PCI NIC card connected to PCIE2 is not working
The pci devices' memory map is wrong. There
should be single memory space for all pcie devices attached
to one controller. the 32 bit aligned and 64 bit aligned
memory spaces should be allocated from this space. The address
returned to caller is the offset from the PCI controller base address.
when a memory read/write request from/to an address comes,
first the Base address of PCI controller is added to address and then
read/write from/to the address is performed.

Contributed-under: TianoCore Contribution Agreement 1.0
Signed-off-by: Pankaj Bansal <pankaj.bansal@nxp.com>

LS1043aRdbPkg/Library/PciHostBridgeLib/PciRbLib.c
@@ -110,9 +110,14 @@ RootBridgeIoMemRW (
     return Status;
   }
 
+  if ((Address < CONFIG_SYS_LS_PCIE_MEM_PHYS_OFF) ||
+      (Address >= (CONFIG_SYS_LS_PCIE_MEM_PHYS_OFF + CONFIG_SYS_LS_PCIE_MEM_SIZE)) {
+    return EFI_INVALID_PARAMETER;
+  }
+
   PrivateData = DRIVER_INSTANCE_FROM_PCI_ROOT_BRIDGE_IO_THIS(This);
 
-  Address = PrivateData -> MemTranslation + Address;
+  Address += PrivateData->PciBaseAddress64;
 
   InStride = mInStride[Width];
   OutStride = mOutStride[Width];
@@ -243,6 +248,11 @@ RootBridgeIoPciRW (
   Status = RootBridgeIoCheckParameter (This, PciOperation, Width, Address, Count, Buffer);
   if (EFI_ERROR (Status)) {
     return Status;
+  }
+
+  if ((Address < CONFIG_SYS_LS_PCIE_IO_PHYS_OFF) ||
+      (Address >= (CONFIG_SYS_LS_PCIE_IO_PHYS_OFF + CONFIG_SYS_LS_PCIE_IO_SIZE)) {
+    return EFI_INVALID_PARAMETER;
   }
 
   PciRbAddr = (EFI_PCI_ROOT_BRIDGE_IO_PROTOCOL_PCI_ADDRESS*) &Address;

LS1043aRdbPkg/Drivers/PciHostBridgeDxe/PciHostBridgeDxe.c
@@ -19,10 +19,6 @@
 
 #include <PciHostBridge.h>
 
-#define PCI1_MEM32_BASE     FixedPcdGet64 (PcdPci1Mmio32Base)
-#define PCI2_MEM32_BASE     FixedPcdGet64 (PcdPci2Mmio32Base)
-#define PCI3_MEM32_BASE     FixedPcdGet64 (PcdPci3Mmio32Base)
-#define PCI_MEM32_SIZE      FixedPcdGet64 (PcdPciMmio32Size)
 #define PCI1_MEM64_BASE     FixedPcdGet64 (PcdPci1Mmio64Base)
 #define PCI2_MEM64_BASE     FixedPcdGet64 (PcdPci2Mmio64Base)
 #define PCI3_MEM64_BASE     FixedPcdGet64 (PcdPci3Mmio64Base)
@@ -209,18 +205,15 @@ PciHostBridgeEntryPoint (
 
       if (Loop1 == 0) {
         PrivateData[Loop1]->DevicePath.AcpiDevicePath.UID = 0;
-        PrivateData[Loop1]->BaseAddress64 = PCI1_MEM64_BASE;
-        PrivateData[Loop1]->BaseAddress32 = PCI1_MEM32_BASE;
+        PrivateData[Loop1]->PciMemBase64 = PCI1_MEM64_BASE;
       }
       if (Loop1 == 1) {
         PrivateData[Loop1]->DevicePath.AcpiDevicePath.UID = 1;
-        PrivateData[Loop1]->BaseAddress64 = PCI2_MEM64_BASE;
-        PrivateData[Loop1]->BaseAddress32 = PCI2_MEM32_BASE;
+        PrivateData[Loop1]->PciMemBase64 = PCI2_MEM64_BASE;
       }
       if (Loop1 == 2) {
         PrivateData[Loop1]->DevicePath.AcpiDevicePath.UID = 2;
-        PrivateData[Loop1]->BaseAddress64 = PCI3_MEM64_BASE;
-        PrivateData[Loop1]->BaseAddress32 = PCI3_MEM32_BASE;
+        PrivateData[Loop1]->PciMemBase64 = PCI3_MEM64_BASE;
       }
 
       PrivateData[Loop1]->Info = AllocateZeroPool (sizeof(struct LsPcieInfo));
@@ -271,23 +264,6 @@ PciHostBridgeEntryPoint (
       DEBUG ((EFI_D_INFO, "Successfully Installed RootBridgeIo and DevicePath protocols\n"));
       InsertTailList (&HostBridge[Loop1]->Head, &PrivateData[Loop1]->Link);
     }
-  }
-
-  // PCI 32bit Memory Space
-  Status = AddMemorySpace(PCI1_MEM32_BASE, PCI_MEM32_SIZE);
-  if (EFI_ERROR (Status)) {
-    DEBUG ((EFI_D_ERROR, "Memory Resource couldn't be added for PCI1_MEM32\n"));
-    return EFI_DEVICE_ERROR;
-  }
-  Status = AddMemorySpace(PCI2_MEM32_BASE, PCI_MEM32_SIZE);
-  if (EFI_ERROR (Status)) {
-    DEBUG ((EFI_D_ERROR, "Memory Resource couldn't be added for PCI2_MEM32\n"));
-    return EFI_DEVICE_ERROR;
-  }
-  Status = AddMemorySpace(PCI3_MEM32_BASE, PCI_MEM32_SIZE);
-  if (EFI_ERROR (Status)) {
-    DEBUG ((EFI_D_ERROR, "Memory Resource couldn't be added for PCI3_MEM32\n"));
-    return EFI_DEVICE_ERROR;
   }
 
   // PCI 64bit Memory Space
@@ -396,8 +372,8 @@ PciNotifyPhase (
       BitsOfAlignment = HighBitSet64 (RootBridgeInstance->ResAlloc[ResTypeMem32].Alignment) + 1;  // Get the number of '1' in Alignment
       AddrLen = RootBridgeInstance->ResAlloc[ResTypeMem32].Length;
 
-      // Top of the 32bit PCI Memory space
-      BaseAddress = RootBridgeInstance->BaseAddress32 + FixedPcdGet32 (PcdPciMmio32Size);
+      // Top of the 64bit PCI Memory space
+      BaseAddress = RootBridgeInstance->PciMemBase64 + PCI_MEM64_SIZE;
 
       Status = gDS->AllocateMemorySpace (
                   EfiGcdAllocateMaxAddressSearchTopDown,
@@ -409,9 +385,9 @@ PciNotifyPhase (
                   NULL
                );
 
-      // Ensure the allocation is in the 32bit PCI memory space
-      if (!EFI_ERROR (Status) && (BaseAddress >= RootBridgeInstance->BaseAddress32)) {
-        RootBridgeInstance->ResAlloc[ResTypeMem32].Base   = (UINTN)BaseAddress;
+      // Ensure the allocation is in the 64bit PCI memory space
+      if (!EFI_ERROR (Status) && (BaseAddress >= RootBridgeInstance->PciMemBase64)) {
+        RootBridgeInstance->ResAlloc[ResTypeMem32].Base   = (UINTN)(BaseAddress - RootBridgeInstance->PciBaseAddress64);
       }
     }
 
@@ -419,8 +395,8 @@ PciNotifyPhase (
       BitsOfAlignment = HighBitSet64 (RootBridgeInstance->ResAlloc[ResTypePMem32].Alignment) + 1;  // Get the number of '1' in Alignment
       AddrLen = RootBridgeInstance->ResAlloc[ResTypePMem32].Length;
 
-      // Top of the 32bit PCI Memory space
-      BaseAddress = RootBridgeInstance->BaseAddress32 + FixedPcdGet64 (PcdPciMmio32Size);
+      // Top of the 64bit PCI Memory space
+      BaseAddress = RootBridgeInstance->PciMemBase64 + PCI_MEM64_SIZE;
 
       Status = gDS->AllocateMemorySpace (
                   EfiGcdAllocateMaxAddressSearchTopDown,
@@ -432,18 +408,18 @@ PciNotifyPhase (
                   NULL
                );
 
-      // Ensure the allocation is in the 32bit PCI memory space
-      if (!EFI_ERROR (Status) && (BaseAddress >= RootBridgeInstance->BaseAddress32)) {
-        RootBridgeInstance->ResAlloc[ResTypePMem32].Base = (UINTN)BaseAddress;
+      // Ensure the allocation is in the 64bit PCI memory space
+      if (!EFI_ERROR (Status) && (BaseAddress >= RootBridgeInstance->PciMemBase64)) {
+        RootBridgeInstance->ResAlloc[ResTypePMem32].Base = (UINTN)(BaseAddress - RootBridgeInstance->PciBaseAddress64);
       }
     }
 
     if (RootBridgeInstance->ResAlloc[ResTypeMem64].Length != 0) {
       BitsOfAlignment = HighBitSet64 (RootBridgeInstance->ResAlloc[ResTypeMem64].Alignment) + 1;  // Get the number of '1' in Alignment
       AddrLen = RootBridgeInstance->ResAlloc[ResTypeMem64].Length;
-
       // Top of the 64bit PCI Memory space
-      BaseAddress = RootBridgeInstance->BaseAddress64 + FixedPcdGet64 (PcdPciMmio64Size);
+      BaseAddress = RootBridgeInstance->PciMemBase64 + PCI_MEM64_SIZE;
+
       Status = gDS->AllocateMemorySpace (
                   EfiGcdAllocateMaxAddressSearchTopDown,
                   EfiGcdMemoryTypeMemoryMappedIo,
@@ -455,16 +431,16 @@ PciNotifyPhase (
                );
 
       // Ensure the allocation is in the 64bit PCI memory space
-      if (!EFI_ERROR (Status) && (BaseAddress >= RootBridgeInstance->BaseAddress64)) {
-        RootBridgeInstance->ResAlloc[ResTypeMem64].Base   = (UINTN)BaseAddress;
+      if (!EFI_ERROR (Status) && (BaseAddress >= RootBridgeInstance->PciMemBase64)) {
+        RootBridgeInstance->ResAlloc[ResTypeMem64].Base   = (UINTN)(BaseAddress - RootBridgeInstance->PciBaseAddress64);
       }
     }
     if (RootBridgeInstance->ResAlloc[ResTypePMem64].Length != 0) {
       BitsOfAlignment = HighBitSet64 (RootBridgeInstance->ResAlloc[ResTypePMem64].Alignment) + 1;  //Get the number of '1' in Alignment
       AddrLen = RootBridgeInstance->ResAlloc[ResTypePMem64].Length;
-
       // Top of the 64bit PCI Memory space
-      BaseAddress = RootBridgeInstance->BaseAddress64 + FixedPcdGet64 (PcdPciMmio64Size);
+      BaseAddress = RootBridgeInstance->PciMemBase64 + PCI_MEM64_SIZE;
+
       Status = gDS->AllocateMemorySpace (
                   EfiGcdAllocateMaxAddressSearchTopDown,
                   EfiGcdMemoryTypeMemoryMappedIo,
@@ -476,8 +452,8 @@ PciNotifyPhase (
                );
 
       // Ensure the allocation is in the 64bit PCI memory space
-      if (!EFI_ERROR (Status) && (BaseAddress >= RootBridgeInstance->BaseAddress64)) {
-        RootBridgeInstance->ResAlloc[ResTypePMem64].Base   = (UINTN)BaseAddress;
+      if (!EFI_ERROR (Status) && (BaseAddress >= RootBridgeInstance->PciMemBase64)) {
+        RootBridgeInstance->ResAlloc[ResTypePMem64].Base   = (UINTN)(BaseAddress - RootBridgeInstance->PciBaseAddress64);
       }
     }
 

LS1043aRdbPkg/Drivers/PciHostBridgeDxe/PciHostBridgeDxe.inf
@@ -53,14 +53,6 @@
   gArmPlatformTokenSpaceGuid.PcdPciBusMax
   gArmPlatformTokenSpaceGuid.PcdPciIoBase
   gArmPlatformTokenSpaceGuid.PcdPciIoSize 
-  gArmPlatformTokenSpaceGuid.PcdPciIoTranslation
-  gArmPlatformTokenSpaceGuid.PcdPciMmio32Size 
-  gArmPlatformTokenSpaceGuid.PcdPci1MemTranslation
-  gArmPlatformTokenSpaceGuid.PcdPci2MemTranslation
-  gArmPlatformTokenSpaceGuid.PcdPci3MemTranslation
-  gArmPlatformTokenSpaceGuid.PcdPci1Mmio32Base
-  gArmPlatformTokenSpaceGuid.PcdPci2Mmio32Base
-  gArmPlatformTokenSpaceGuid.PcdPci3Mmio32Base
   gArmPlatformTokenSpaceGuid.PcdPci1Mmio64Base
   gArmPlatformTokenSpaceGuid.PcdPci2Mmio64Base
   gArmPlatformTokenSpaceGuid.PcdPci3Mmio64Base

LS1043aRdbPkg/Drivers/PciHostBridgeDxe/PciRootBridgeIo.c
@@ -680,32 +680,30 @@ PciRbInitialize (
   }
 
   if (HostNo == 1) {
-    if (PcdGet64 (PcdPci1ExpressBaseAddress) == 0) {
+    PrivateData->PciBaseAddress64 = PcdGet64 (PcdPci1ExpressBaseAddress); 
+    if (!PrivateData->PciBaseAddress64) {
       DEBUG ((EFI_D_ERROR, "%a: PCI 1 host bridge not present\n", __FUNCTION__));
       return EFI_ABORTED;
     }
 
     PcieSetupCntrl(PrivateData, PrivateData->Pcie, PrivateData->Info);
-    PrivateData->MemTranslation = PcdGet64 (PcdPci1MemTranslation);
   } else if (HostNo == 2 ) { 
-    if (PcdGet64 (PcdPci2ExpressBaseAddress) == 0) {
+    PrivateData->PciBaseAddress64 = PcdGet64 (PcdPci2ExpressBaseAddress); 
+    if (!PrivateData->PciBaseAddress64) {
       DEBUG ((EFI_D_ERROR, "%a: PCI 2 host bridge not present\n", __FUNCTION__));
       return EFI_ABORTED;
     }
 
     PcieSetupCntrl(PrivateData, PrivateData->Pcie, PrivateData->Info);
-    PrivateData->MemTranslation = PcdGet64 (PcdPci2MemTranslation);
   } else if (HostNo == 3 ) { 
-    if (PcdGet64 (PcdPci3ExpressBaseAddress) == 0) {
+    PrivateData->PciBaseAddress64 = PcdGet64 (PcdPci3ExpressBaseAddress); 
+    if (!PrivateData->PciBaseAddress64) {
       DEBUG ((EFI_D_ERROR, "%a: PCI 3 host bridge not present\n", __FUNCTION__));
       return EFI_ABORTED;
     }
 
     PcieSetupCntrl(PrivateData, PrivateData->Pcie, PrivateData->Info);
-    PrivateData->MemTranslation = PcdGet64 (PcdPci3MemTranslation);
   }
-
-  PrivateData->IoTranslation = PcdGet64 (PcdPciIoTranslation);
 
   PrivateData->BusStart  = FixedPcdGet32 (PcdPciBusMin);
   PrivateData->BusLength = FixedPcdGet32 (PcdPciBusMax) - FixedPcdGet32 (PcdPciBusMin) + 1;

LS1043aRdbPkg/Include/LS1043aRdb.h
@@ -178,9 +178,9 @@
 #define CONFIG_SYS_LS_PCIE_IO_PHYS_OFF     0x00010000
 #define CONFIG_SYS_LS_PCIE_IO_SIZE         0x00010000      /* 64k */
 
-#define CONFIG_SYS_LS_PCIE_MEM_BUS         0x30000000
-#define CONFIG_SYS_LS_PCIE_MEM_PHYS_OFF    0x30000000
-#define CONFIG_SYS_LS_PCIE_MEM_SIZE        0x08000000      /* 128 MB */
+#define CONFIG_SYS_LS_PCIE_MEM_BUS         0x40000000
+#define CONFIG_SYS_LS_PCIE_MEM_PHYS_OFF    0x40000000
+#define CONFIG_SYS_LS_PCIE_MEM_SIZE        0x40000000      /* 1 GB */
 
 #ifdef CONFIG_LS_PCI
 #define CONFIG_NET_MULTI

LS1043aRdbPkg/Include/PciRootBridge.h
@@ -113,10 +113,8 @@ typedef struct {
   UINTN                  BusLength;
   UINT64                 MemAllocAttributes;
   PCI_RESOURCE_ALLOC     ResAlloc[ResTypeMax];
-  UINT64	  	IoTranslation;
-  UINT64                MemTranslation;
-  UINT64                BaseAddress64;
-  UINT32                BaseAddress32;
+  UINT64                PciBaseAddress64;
+  UINT64                PciMemBase64;
   struct LsPcieInfo	*Info;
   struct LsPcie		*Pcie;
   struct PCI_ROOT_BRIDGE_INSTANCE *Next;

LS1043aRdbPkg/LS1043aRdbPkg.dec
@@ -153,19 +153,10 @@
   gArmPlatformTokenSpaceGuid.PcdPciBusMax|255|UINT64|0x00000043
   gArmPlatformTokenSpaceGuid.PcdPciIoBase|0x00010000|UINT64|0x00000044
   gArmPlatformTokenSpaceGuid.PcdPciIoSize|0x00010000|UINT64|0x00000045
-  gArmPlatformTokenSpaceGuid.PcdPciMemTranslation|0x5000000000|UINT64|0x00000055
-  gArmPlatformTokenSpaceGuid.PcdPci1Mmio32Base|0x28000000|UINT32|0x00000065
-  gArmPlatformTokenSpaceGuid.PcdPci2Mmio32Base|0x38000000|UINT32|0x00000066
-  gArmPlatformTokenSpaceGuid.PcdPci3Mmio32Base|0x30000000|UINT32|0x00000046
-  gArmPlatformTokenSpaceGuid.PcdPciMmio32Size|0x08000000|UINT32|0x00000047
-  gArmPlatformTokenSpaceGuid.PcdPci1MemTranslation|0x4000000000|UINT64|0x00000067
-  gArmPlatformTokenSpaceGuid.PcdPci2MemTranslation|0x4800000000|UINT64|0x00000068
-  gArmPlatformTokenSpaceGuid.PcdPci3MemTranslation|0x5000000000|UINT64|0x00000069
   gArmPlatformTokenSpaceGuid.PcdPci1Mmio64Base|0x4040000000|UINT64|0x0000006A
   gArmPlatformTokenSpaceGuid.PcdPci2Mmio64Base|0x4840000000|UINT64|0x0000006B
   gArmPlatformTokenSpaceGuid.PcdPci3Mmio64Base|0x5040000000|UINT64|0x0000006C
   gArmPlatformTokenSpaceGuid.PcdPciMmio64Size|0x0040000000|UINT64|0x00000049
-  gArmPlatformTokenSpaceGuid.PcdPciIoTranslation|0x00010000|UINT64|0x00000050
   gEfiMdePkgTokenSpaceGuid.PcdPci1ExpressBaseAddress|0x4000000000|UINT64|0x00000051
   gEfiMdePkgTokenSpaceGuid.PcdPci2ExpressBaseAddress|0x4800000000|UINT64|0x00000052
   gEfiMdePkgTokenSpaceGuid.PcdPci3ExpressBaseAddress|0x5000000000|UINT64|0x00000058

LS1043aRdbPkg/LS1043aRdbPkg.dsc
@@ -420,14 +420,6 @@
   gArmPlatformTokenSpaceGuid.PcdPciBusMax|255
   gArmPlatformTokenSpaceGuid.PcdPciIoBase|0x00010000
   gArmPlatformTokenSpaceGuid.PcdPciIoSize|0x00010000      # 64k
-  gArmPlatformTokenSpaceGuid.PcdPciIoTranslation|0x00010000
-  gArmPlatformTokenSpaceGuid.PcdPci1Mmio32Base|0x28000000
-  gArmPlatformTokenSpaceGuid.PcdPci2Mmio32Base|0x38000000
-  gArmPlatformTokenSpaceGuid.PcdPci3Mmio32Base|0x30000000
-  gArmPlatformTokenSpaceGuid.PcdPciMmio32Size|0x08000000
-  gArmPlatformTokenSpaceGuid.PcdPci1MemTranslation|0x4000000000
-  gArmPlatformTokenSpaceGuid.PcdPci2MemTranslation|0x4800000000
-  gArmPlatformTokenSpaceGuid.PcdPci3MemTranslation|0x5000000000
   gArmPlatformTokenSpaceGuid.PcdPci1Mmio64Base|0x4040000000
   gArmPlatformTokenSpaceGuid.PcdPci2Mmio64Base|0x4840000000
   gArmPlatformTokenSpaceGuid.PcdPci3Mmio64Base|0x5040000000

commit b259be1db6eebff9cd4e7d02a33c654a8f8b8a62

LS1043 : Fix PCI error introduced inadvertently in commit 17d39cabee3eab5935c60d7e96c272c4e1e8c4e4
The IO space Address range checking was added in PCI
configuration space read/write function, while it should
have been added in IO space read/write function (which is
not implemented yet).

Contributed-under: TianoCore Contribution Agreement 1.0
Signed-off-by: Pankaj Bansal <pankaj.bansal@nxp.com>

LS1043aRdbPkg/Library/PciHostBridgeLib/PciRbLib.c
@@ -254,12 +254,6 @@ RootBridgeIoPciRW (
     return Status;
   }
 
-  if ((Address < CONFIG_SYS_LS_PCIE_IO_PHYS_OFF) ||
-      (Address >= ((UINT32)CONFIG_SYS_LS_PCIE_IO_PHYS_OFF +
-                   CONFIG_SYS_LS_PCIE_IO_SIZE))) {
-    return EFI_INVALID_PARAMETER;
-  }
-
   PciRbAddr = (EFI_PCI_ROOT_BRIDGE_IO_PROTOCOL_PCI_ADDRESS*) &Address;
 
   if (PciRbAddr->ExtendedRegister)