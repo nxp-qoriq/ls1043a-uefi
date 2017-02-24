ce3dd265aa92e8fe84f7d5ce40cfe751bebbd007
Pankaj Bansal <pankaj.bansal@nxp.com>
2017-02-24 14:35:38 +0530

    QUEFI-470: Fix RuntimeServicesTest->TimeServicesTest Faliures
    Issue: TimeServicesTest in RuntimeServicesTest of SCT tests failing
    Cause: Paramters are not being checked before set/get time
    Fix: add paramter checking before get/set time.
    
    Contributed-under: TianoCore Contribution Agreement 1.0
    Signed-off-by: Pankaj Bansal <pankaj.bansal@nxp.com>

--- LS1043aRdbPkg/Library/Ds1307RtcLib/Ds1307RtcLib.c
+++ LS1043aRdbPkg/Library/Ds1307RtcLib/Ds1307RtcLib.c
@@ -81,6 +81,43 @@ VOID RtcWrite(
 
 }
 
+BOOLEAN
+IsLeapYear (
+  IN EFI_TIME   *Time
+  )
+{
+  if (Time->Year % 4 == 0) {
+    if (Time->Year % 100 == 0) {
+      if (Time->Year % 400 == 0) {
+        return TRUE;
+      } else {
+        return FALSE;
+      }
+    } else {
+      return TRUE;
+    }
+  } else {
+    return FALSE;
+  }
+}
+
+BOOLEAN
+DayValid (
+  IN  EFI_TIME  *Time
+  )
+{
+  STATIC CONST INTN DayOfMonth[12] = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+  if (Time->Day < 1 ||
+      Time->Day > DayOfMonth[Time->Month - 1] ||
+      (Time->Month == 2 && (!IsLeapYear (Time) && Time->Day > 28))
+     ) {
+    return FALSE;
+  }
+
+  return TRUE;
+}
+
 /**
   Returns the current time and date information, and the time-keeping capabilities
   of the hardware platform.
@@ -108,6 +145,9 @@ LibGetTime (
   CHAR16      Second, Minute, Hour, Day, Month, Year;
   UINTN       Size;
 
+  if (Time == NULL)
+    return EFI_INVALID_PARAMETER;
+
   Second = RtcRead (DS1307_SEC_REG_ADDR);
   Minute = RtcRead (DS1307_MIN_REG_ADDR);
   Hour = RtcRead (DS1307_HR_REG_ADDR);
@@ -124,12 +164,13 @@ LibGetTime (
     goto EXIT;
   }
 
+  Time->Nanosecond = 0;
   Time->Second  = Bin (Second & 0x7F);
   Time->Minute  = Bin (Minute & 0x7F);
   Time->Hour = Bin (Hour & 0x3F);
   Time->Day = Bin (Day & 0x3F);
   Time->Month  = Bin (Month & 0x1F);
-  Time->Year = Bin (Year) + ( Bin (Year) >= 70 ? 1900 : 2000);
+  Time->Year = Bin (Year) + ( Bin (Year) >= 98 ? 1900 : 2000);
 
   // Get the current time zone information from non-volatile storage
   Size = sizeof (TimeZone);
@@ -242,8 +283,25 @@ LibSetTime (
 {
   EFI_STATUS  Status;
 
-  if (Time->Year < 1970 || Time->Year > 2069)
-    DEBUG((EFI_D_ERROR, "WARNING: Year should be between 1970 and 2069!\n"));
+  // Check the input parameters are within the range specified by UEFI
+  if ((Time->Year   < 1900) ||
+       (Time->Year   > 9999) ||
+       (Time->Month  < 1   ) ||
+       (Time->Month  > 12  ) ||
+       (!DayValid (Time)    ) ||
+       (Time->Hour   > 23  ) ||
+       (Time->Minute > 59  ) ||
+       (Time->Second > 59  ) ||
+       (Time->Nanosecond > 999999999) ||
+       (!((Time->TimeZone == EFI_UNSPECIFIED_TIMEZONE) || ((Time->TimeZone >= -1440) && (Time->TimeZone <= 1440)))) ||
+       (Time->Daylight & (~(EFI_TIME_ADJUST_DAYLIGHT | EFI_TIME_IN_DAYLIGHT)))
+    ) {
+    Status = EFI_INVALID_PARAMETER;
+    goto EXIT;
+  }
+
+  if (Time->Year < 1998 || Time->Year > 2097)
+    DEBUG((EFI_D_ERROR, "WARNING: Year should be between 1998 and 2097!\n"));
 
   RtcWrite (DS1307_YR_REG_ADDR, Bcd (Time->Year % 100));
   RtcWrite (DS1307_MON_REG_ADDR, Bcd (Time->Month));
